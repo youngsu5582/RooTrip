a8748ae6d24fa3ea4df63a9ba6b4eed71792e2db
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-24 11:57:17 +0900

    feat(Husky) : 커밋 전 Lint 위한 pre-commit Hook 추가

---
+++ link-staged.config.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+module.exports = {
+    '*.(md|yaml|yml)': ['prettier --write'],
+    '*.+(ts|js|json)': ['eslint --fix', 'prettier --write'],
+    '*.+(ts)': [() => 'tsc -p tsconfig.lib.json --noEmit', () => 'tsc -p tsconfig.spec.json --noEmit'],
+};
