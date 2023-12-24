27f19a9b54a93fdb8fac836b06b287ea2967d1bd
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-12-24 17:50:51 +0900

    feat(Jest) : 테스팅 위한 Config , Setup 추가

---
+++ jest.config.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable import/no-default-export */
+import type { Config } from 'jest';
+
+const config: Config = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    transform: {
+        '^.+\\.spec\\.(t|j)s$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/tsconfig.json',
+            },
+        ],
+        '^.+\\.(t|j)s$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.json',
+            },
+        ],
+    },
+    testTimeout: 120_000,
+    testEnvironment: 'node',
+    verbose: true,
+    detectLeaks: false,
+    detectOpenHandles: true,
+    collectCoverage: true,
+    collectCoverageFrom: ['**/*.ts', '!**/*.d.ts'],
+    coverageThreshold: {
+        global: {
+            statements: 60,
+            branches: 60,
+            functions: 60,
+            lines: 60,
+        },
+    },
+    coveragePathIgnorePatterns: ['<rootDir>/jest.config.ts', '.mock.ts', 'spec/'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+export default config;
