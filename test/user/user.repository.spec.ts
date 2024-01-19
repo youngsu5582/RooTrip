5febdb5d61f23450bc500bb82cc443b5e5718287
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-01-19 15:28:57 +0900

    feat : 리프레쉬 토큰 저장 , 이메일 통한 유저 검색 기능 구현
    리프레쉬 토큰을 저장해주는 saveRefreshToken
    Email 통해 User 검색하는 findUserDomainByEmail
    이에 따른 테스트 코드 추가

--- test/user/user.repository.spec.ts
+++ test/user/user.repository.spec.ts
@@ -1,21 +1,52 @@
 import { Test } from '@nestjs/testing';
+import { UserDomain } from 'src/user/domain/user.domain';
 import { UserRepository } from 'src/user/repository/user.repository';
 import { UserModule } from 'src/user/user.module';
 import { UserFixture } from 'test/fixture/user.fixture';
 
 describe('UserRepository', () => {
     let userRepository: UserRepository;
-    beforeEach(async () => {
+    beforeAll(async () => {
         const module = await Test.createTestingModule({
             imports: [UserModule],
         }).compile();
 
         userRepository = module.get(UserRepository);
     });
-    it('UserDomain 을 통해 , DB 에 User 를 삽입후 True 를 반환한다.', async () => {
+    beforeEach(async () => {
         const userDomain = UserFixture.getDomain();
+        await userRepository.createUser(userDomain);
+    });
+    afterEach(async () => {
+        const userDomain = UserFixture.getDomain();
+        await userRepository.deleteUser(userDomain);
+    });
+    it('UserDomain 을 통해 , DB 에 User 를 삽입후 True 를 반환한다.', async () => {
+        const userDomain = UserFixture.getDomain(1);
 
         const result = await userRepository.createUser(userDomain);
+
+        expect(result).toBeTruthy();
+    });
+    it('email 을 통해 , DB 에서 Email 이 일치하는 UserDomain 을 받아온다.', async () => {
+        const email = UserFixture.getDomain().user.email;
+
+        const userDomain = await userRepository.findUserDomainByEmail(email);
+
+        expect(() => userDomain.validEmail(email)).not.toThrow();
+    });
+    it('DB에서 Email이 일치하는 UserDomain 이 없는 경우 익명 유저를 받아온다.', async () => {
+        const notExistedEmail = 'notExistedEmail@gmail.com';
+
+        const userDomain = await userRepository.findUserDomainByEmail(notExistedEmail);
+
+        expect(userDomain.user.email).toEqual(UserDomain.getAnonymousUser().user.email);
+    });
+    it('UserDomain 을 통해 , DB에 해당하는 User 에서 refreshToken Column 을 수정하고 True 를 반환한다.', async () => {
+        const refreshToken = 'eyxuiuasn213';
+        const userDomain = UserFixture.getDomain();
+
+        const result = await userRepository.saveRefreshToken(userDomain, refreshToken);
 
         expect(result).toBeTruthy();
     });
